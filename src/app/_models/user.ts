d3b8665578f6781e1c93d918cd05aa38d1abfe80
menilpatel <167094143+menilpatel@users.noreply.github.com>
2024-04-24 18:40:40 +0530

    commit

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,9 +1,11 @@
 export class User {
     id?: string;
+    role?: string;
     email?: string;
     password?: string;
     firstName?: string;
     middleName?: string;
     lastName?: string;
+    image? : string;
     token?: string;
 }
